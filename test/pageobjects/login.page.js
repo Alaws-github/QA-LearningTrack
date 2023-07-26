4cb5e50d4450783e6ba9973e3e2f740cbe9d4c41
AndrewLawrence <starandy1@gmail.com>
2023-07-26 05:38:37 +0500

    login and wait for OTP

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -10,16 +10,25 @@ class LoginPage extends Page {
      * define selectors using getter methods
      */
     get inputUsername () {
-        return $('#username');
+        return $('#i0116');
     }
 
     get inputPassword () {
-        return $('#password');
+        return $('#i0118');
+    }
+    get btnSubmit () {
+     
+        return $('input[type="submit"]');
     }
 
-    get btnSubmit () {
-        return $('button[type="submit"]');
+    get selectVerificationCOde () {
+        return $('.row:nth-child(1) .text-left > div');
     }
+
+    get inputOTP () {
+        return $('#idTxtBx_SAOTCC_OTC');
+    }
+
 
     /**
      * a method to encapsule automation code to interact with the page
@@ -27,15 +36,21 @@ class LoginPage extends Page {
      */
     async login (username, password) {
         await this.inputUsername.setValue(username);
+        await this.btnSubmit.click();
+        await browser.pause(3000)
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
+        await browser.pause(3000)
+        await this.selectVerificationCOde.click();
+        await browser.pause(15000)
+        
     }
 
     /**
      * overwrite specific options to adapt it to page object
      */
     open () {
-        return super.open('login');
+        return super.open();
     }
 }
 
